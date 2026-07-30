9224b1803e656b3a2eb35fa5f4438fa0a5fdf9db
Adam Ali <adamwaliditpro@gmail.com>
2026-07-30 22:38:30 +0200

    feat: configure API startup with DI, EF Core, and Scalar

--- Bookify/Program.cs
+++ Bookify/Program.cs
@@ -1,3 +1,11 @@
+using Bookify.Application.Interfaces;
+using Bookify.Application.Services;
+using Bookify.Domain.Interfaces;
+using Bookify.Infrastructure.Data;
+using Bookify.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,16 +13,34 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+	options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
+builder.Services.AddScoped<IResourceRepository, ResourceRepository>();
+builder.Services.AddScoped<IBookingRepository, BookingRepository>();
+builder.Services.AddScoped<ICustomerService, CustomerService>();
+builder.Services.AddScoped<IResourceService, ResourceService>();
+builder.Services.AddScoped<IBookingService, BookingService>();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+	var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+	dbContext.Database.EnsureCreated();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+	app.MapScalarApiReference();
 	app.MapOpenApi();
 }
 
 app.UseHttpsRedirection();
+
+
 
 app.UseAuthorization();
 
